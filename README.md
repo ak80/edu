74e9f0fd056c08ac3f70b580a5b819f783e42733
Alexander Koch <josef.koch@gmail.com>
2015-04-25 08:13:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# edu
+educational
+
+ * [Hamcrest](https://github.com/ak80/edu/blob/master/src/test/java/HamcrestSamples.java)
---
+++ src/test/java/HamcrestSamples.java
@@ -0,0 +1,100 @@
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.collection.IsMapContaining.hasKey;
+
+public class HamcrestSamples {
+
+
+  @Test
+  public void usingHamcrest() {
+    // add identifier
+    assertThat("identifier", "foo", equalTo("foo"));
+
+    // core
+    assertThat("foo", is(anything("bar")));
+    assertThat("foo", is("foo"));
+    assertThat("foo", is(equalTo("foo")));
+
+    // logical
+    for (int i = 1; i < 4; i++) {
+      assertThat(1, anyOf(equalTo(1), equalTo(2), equalTo(3)));
+    }
+    assertThat("foo", allOf(startsWith("f"), containsString("o"), endsWith("o")));
+    assertThat("foo", is(not("bar")));
+
+    // object
+    assertThat("foo", equalTo("foo"));
+    assertThat(true, hasToString(equalTo("true")));
+    assertThat("foo", instanceOf(String.class));
+    assertThat("foo", notNullValue());
+    assertThat(null, nullValue());
+    assertThat("foo", sameInstance("foo"));
+
+    // beans
+    assertThat(new Date(), hasProperty("time"));
+
+    //collection
+    Map<String, String> map = new HashMap<>();
+    map.put("foo", "bar");
+    assertThat(map, hasEntry("foo", "bar"));
+    assertThat(map, hasKey("foo"));
+    assertThat(map, hasValue("bar"));
+
+    List<String> list = Arrays.asList(new String[]{"foo", "bar", "bazz"});
+    assertThat(list, hasItem(is("foo")));
+    assertThat(list, hasItems(is("bar"), is("foo")));
+    assertThat(list, hasSize(3));
+    assertThat(list.toArray(), hasItemInArray("foo"));
+
+    // number
+    assertThat(5.501, closeTo(5.502, 0.01));
+    assertThat(5, greaterThan(4));
+    assertThat(5, greaterThanOrEqualTo(4));
+    assertThat(5, greaterThanOrEqualTo(5));
+    assertThat(4, lessThan(5));
+    assertThat(4, lessThanOrEqualTo(4));
+    assertThat(4, lessThanOrEqualTo(5));
+
+    // text
+    assertThat("foo", equalToIgnoringCase("FOO"));
+    assertThat("foo", equalToIgnoringWhiteSpace(" foo"));
+    assertThat("bar", containsString("a"));
+    assertThat("bar", startsWith("b"));
+    assertThat("bar", endsWith("r"));
+    assertThat("", isEmptyString());
+    assertThat(null, isEmptyOrNullString());
+
+  }
+
+  // custom matcher
+  private Matcher<Long> isEqualToDateLong(final Long expectedValue) {
+    final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yy:HH:mm:SS Z");
+
+    return new BaseMatcher<Long>() {
+      @Override
+      public boolean matches(final Object actualValue) {
+        return expectedValue.equals(actualValue);
+      }
+      @Override
+      public void describeTo(final Description description) {
+        description.appendText("was ").appendValue(DATE_FORMAT.format(new Date(expectedValue)));
+      }
+      @Override
+      public void describeMismatch(final Object actualValue, final Description description) {
+        description.appendText("was ").appendValue(DATE_FORMAT.format(new Date((Long) actualValue)));
+      }
+    };
+  }
+
+}
+
+
+
